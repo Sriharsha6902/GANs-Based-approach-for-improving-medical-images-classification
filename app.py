c1a662b5ca47167c3dd05c5e7975ef511b980428
Sriharsha6902 <95066380+Sriharsha6902@users.noreply.github.com>
2024-03-20 15:16:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,28 @@
 import streamlit as st
-import requests
 from PIL import Image
-import numpy as np
-import tensorflow as tf
+import requests
 from io import BytesIO
 
-st.set_page_config(page_icon=":sunny:", layout="wide")
-
-# Function to load and preprocess the image
-def preprocess_image(img):
-    img = img.convert('RGB')
-    img = img.resize((224, 224))
-    img = np.array(img)
-    img = img / 255
-    return img
-
-# Load your pre-trained model
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model = tf.keras.models.load_model('classifier_pneumonia_224x224.h5')
-    return model
-
-# Function to make predictions
-def predict(image, model):
-    img = preprocess_image(image)
-    img = np.expand_dims(img, axis=0)
-    prediction = model.predict(img)
-    return prediction
-
 def main():
-    st.title("Image Classification for Pneumonia")
+    st.title("Pneumonia Detection App")
 
-    # Load the image from URL
-    image_url = "https://i.postimg.cc/mgcdTtQm/78205958-d81f-434a-8a70-7f5a00f12645.jpg"
-    response = requests.get(image_url)
-    img = Image.open(BytesIO(response.content))
+    # Load the image from the URL
+    response = requests.get('https://i.postimg.cc/mgcdTtQm/78205958-d81f-434a-8a70-7f5a00f12645.jpg')
+    image = Image.open(BytesIO(response.content))
 
-    # Display the image to the right of the title
-    col1, col2 = st.columns([1, 2])
-    with col1:
-        st.write(" ")
-    with col2:
-        st.image(img, caption="Uploaded Image", use_column_width=True)
+    # Create columns
+    col1, col2 = st.beta_columns(2)
 
-    # Make predictions
-    model = load_model()
-    prediction = predict(img, model)
-    y_pred = tf.squeeze(prediction)
-    y_pred = y_pred >= 0.855
-    if y_pred:  # Assuming the first class is normal and second class is pneumonia
-        st.write("Prediction: Pneumonia")
-    else:
-        st.write("Prediction: Normal")
+    # Display the image in the left column
+    col1.image(image, use_column_width=True)
+
+    # Display the title and file uploader in the right column
+    col2.header("Upload your image")
+    uploaded_file = col2.file_uploader("Choose an image...", type="jpg")
+
+    if uploaded_file is not None:
+        user_image = Image.open(uploaded_file)
+        col2.image(user_image, caption='Uploaded Image.', use_column_width=True)
 
 if __name__ == "__main__":
     main()
