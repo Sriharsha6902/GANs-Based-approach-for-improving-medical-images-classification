1796d21d77ca2ea7b1fb1811555ef214069cc87d
Sriharsha6902 <95066380+Sriharsha6902@users.noreply.github.com>
2024-03-20 14:18:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import tensorflow as tf
-# import cv2
+import cv2
 
 # # Function to load and preprocess the image
 def preprocess_image(img):
