77ae11eea78711898154769c9ee07865ed02bd11
Sriharsha6902 <95066380+Sriharsha6902@users.noreply.github.com>
2024-03-20 15:22:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
-import streamlit as st
 from PIL import Image
 import requests
 from io import BytesIO
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+import cv2
+st.set_page_config(page_icon=":sunny:", layout="centered")
+
+# # Function to load and preprocess the image
+def preprocess_image(img):
+    img = Image.open(img).convert('RGB')
+    img = img.resize((224, 224))
+    img = np.array(img)
+    img = img / 255
+    print(img.shape)
+    return img
+
+# # Load your pre-trained model
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = tf.keras.models.load_model('classifier_pneumonia_224x224.h5')
+    return model
+
+# # Function to make predictions
+def predict(image, model):
+    img = preprocess_image(image)
+    img = np.expand_dims(img, axis=0)
+    prediction = model.predict(img)
+    return prediction
+
+
+def main():
+    st.title("Image Classification for Pneumonia")
+    st.write("Upload an image for Pneumonia:")
+    
 
 def main():
     st.title("Pneumonia Detection App")
@@ -17,12 +49,22 @@ def main():
     col1.image(image, use_column_width=True)
 
     # Display the title and file uploader in the right column
-    col2.header("Upload your image")
-    uploaded_file = col2.file_uploader("Choose an image...", type="jpg")
+    st.write("Upload an image for Pneumonia:")
+    uploaded_pneumonia_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], key="pneumonia")
 
-    if uploaded_file is not None:
-        user_image = Image.open(uploaded_file)
-        col2.image(user_image, caption='Uploaded Image.', use_column_width=True)
+    if uploaded_pneumonia_image is not None:
+        model = load_model()
+        prediction = predict(uploaded_pneumonia_image, model)
+        y_pred = tf.squeeze(prediction)
+        print(y_pred)
+        y_pred = y_pred >= 0.855
+        if y_pred:  # Assuming the first class is brain tumor and second class is pneumonia
+            st.write("Prediction: Pneumonia")
+        else:
+            st.write("Prediction: Normal")
+    else :
+        st.write("Please upload the correct file extension")
+        
 
 if __name__ == "__main__":
     main()
