7449bc43689de2288cf0c54991b5783c73d62505
Sriharsha6902 <95066380+Sriharsha6902@users.noreply.github.com>
2024-03-20 15:32:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,30 @@
+import streamlit as st
 from PIL import Image
 import requests
 from io import BytesIO
-import streamlit as st
-import numpy as np
 import tensorflow as tf
-import cv2
-st.set_page_config(page_icon=":sunny:", layout="centered")
+import numpy as np
 
-# # Function to load and preprocess the image
-def preprocess_image(img):
-    img = Image.open(img).convert('RGB')
-    img = img.resize((224, 224))
-    img = np.array(img)
-    img = img / 255
-    print(img.shape)
-    return img
+# Set page config
+st.set_page_config(page_title="Pneumonia Detection App", page_icon=":microscope:", layout="centered", initial_sidebar_state="expanded")
 
-# # Load your pre-trained model
 @st.cache(allow_output_mutation=True)
 def load_model():
     model = tf.keras.models.load_model('classifier_pneumonia_224x224.h5')
     return model
 
-# # Function to make predictions
+def preprocess_image(img):
+    img = Image.open(img).convert('RGB')
+    img = img.resize((224, 224))
+    img = np.array(img)
+    img = img / 255
+    return img
+
 def predict(image, model):
     img = preprocess_image(image)
     img = np.expand_dims(img, axis=0)
     prediction = model.predict(img)
     return prediction
-
-
-def main():
-    st.title("Image Classification for Pneumonia")
-    st.write("Upload an image for Pneumonia:")
-    
 
 def main():
     st.title("Pneumonia Detection App")
@@ -53,18 +44,17 @@ def main():
     uploaded_pneumonia_image = col2.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], key="pneumonia")
 
     if uploaded_pneumonia_image is not None:
-        model = load_model()
-        prediction = predict(uploaded_pneumonia_image, model)
-        y_pred = tf.squeeze(prediction)
-        print(y_pred)
-        y_pred = y_pred >= 0.855
-        if y_pred:  # Assuming the first class is brain tumor and second class is pneumonia
-            col2.write("Prediction: Pneumonia")
-        else:
-            col2.write("Prediction: Normal")
+        with st.spinner('Predicting...'):
+            model = load_model()
+            prediction = predict(uploaded_pneumonia_image, model)
+            y_pred = tf.squeeze(prediction)
+            y_pred = y_pred >= 0.88
+            if y_pred:  # Assuming the first class is brain tumor and second class is pneumonia
+                st.success("Prediction: Pneumonia")
+            else:
+                st.success("Prediction: Normal")
     else :
         st.write("Please upload the correct file extension")
-        
 
 if __name__ == "__main__":
     main()
