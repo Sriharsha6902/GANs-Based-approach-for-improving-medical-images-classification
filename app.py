fd15653cf0fa38ab9670645f2887a38881aeafa9
Sriharsha6902 <95066380+Sriharsha6902@users.noreply.github.com>
2024-03-20 15:35:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,15 +33,22 @@ def main():
     response = requests.get('https://i.postimg.cc/mgcdTtQm/78205958-d81f-434a-8a70-7f5a00f12645.jpg')
     image = Image.open(BytesIO(response.content))
 
-    # Create columns
-    col1, col2 = st.columns(2)
+    # Set the image as the background using custom CSS
+    st.markdown(
+        f"""
+        <style>
+        .reportview-container {{
+            background: url(data:image/png;base64,{image});
+            background-size: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
-    # Display the image in the left column
-    col1.image(image, use_column_width=True)
-
-    # Display the title and file uploader in the right column
-    col2.header("Upload an image for Pneumonia:")
-    uploaded_pneumonia_image = col2.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], key="pneumonia")
+    # Display the title and file uploader
+    st.header("Upload an image for Pneumonia:")
+    uploaded_pneumonia_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], key="pneumonia")
 
     if uploaded_pneumonia_image is not None:
         with st.spinner('Predicting...'):
