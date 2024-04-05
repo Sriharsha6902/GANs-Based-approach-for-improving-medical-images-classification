53c30bb6adf41fd74b451763536f75e1b691fa58
Sriharsha6902 <95066380+Sriharsha6902@users.noreply.github.com>
2024-04-05 16:52:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def main():
                     alzheimers_model = load_alzheimers_model()
                     alzheimers_prediction = predict_alzheimers(uploaded_image, alzheimers_model)
                     pred = np.argmax(alzheimers_prediction,axis=1)
-                    st.success("Prediction: {alz_classes[pred]}")
+                    st.success("Prediction: ",alz_classes[pred])
 
         else:
             st.write("Please upload the correct file extension")
